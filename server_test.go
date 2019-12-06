195b3010c9a4a42e421c9b805edc03804780d57f
João Marques Gomes <joao.m.gomes@celfinet.com>
2019-12-06 16:50:47 +0000

    Fix tests with tenant md5 string

--- server_test.go
+++ server_test.go
@@ -16,8 +16,8 @@ import (
 
 var (
 	machpTest       = "machp:machp123@tcp(localhost:3306)/machp_dev"
-	tenantJSONTom   = `{"id":1,"name":"tom","md5":""}`
-	tenantJSONJerry = `{"id":1,"name":"jerry","md5":""}`
+	tenantJSONTom   = `{"id":1,"name":"tom","md5":"34b7da764b21d298ef307d04d8152dc5"}`
+	tenantJSONJerry = `{"id":1,"name":"jerry","md5":"34b7da764b21d298ef307d04d8152dc5"}`
 )
 
 func dumpJSON(rec *httptest.ResponseRecorder) string {
@@ -48,12 +48,13 @@ func TestTenant(t *testing.T) {
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
+	var cfg Config
 	db, err := sql.Open("mysql", machpTest)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
-	h := &Handler{db}
+	h := &Handler{db, &cfg}
 
 	// createTenant tom
 	if assert.NoError(t, h.createTenant(c)) {
